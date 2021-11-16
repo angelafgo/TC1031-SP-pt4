5558588c3b9d90dad7f169ad0d09cd5dc9d7c9af
angelafgo <75437728+angelafgo@users.noreply.github.com>
2021-11-16 11:59:29 -0600

    creación del main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+
+*
+El presente programa tiene como propósito...
+*/
+
+// Integrantes (Equipo 12) :
+// Daniel Evaristo Escalera Bonilla - A00831289
+// José Arnoldo Rositas Salinas - A00829760
+// Ángela Felicia Guajardo Olveda - A00829821
+
+// SP TC1031
+// 16 Nov 2021
+
+
+int main
+{
+
+}
